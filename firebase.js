8b6829a35358558bc83502387351e6cf43e51637
Graciete2024 <gracietemiguelsebastiaocabeia@gmail.com>
2025-05-03 13:01:54 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,23 @@
-// firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// Importação do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
+// Configuração real do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD3voRUhhaJUSiYUc7WnsN8iMXsNn84UNM",
   authDomain: "darling-clica-e-ganha.firebaseapp.com",
-  projectId: "querida-clica-e-ganha",
-  storageBucket: "darling-clica-e-ganha.appspot.com",
+  projectId: "darling-clica-e-ganha",
+  storageBucket: "darling-clica-e-ganha.firebasestorage.app",
   messagingSenderId: "993176761999",
   appId: "1:993176761999:web:d6a10be0f2e4f42237e2fa",
   measurementId: "G-64JM6D3QS2"
 };
 
+// Inicialização do Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Exportar para uso nos outros arquivos
 export { auth, db };
